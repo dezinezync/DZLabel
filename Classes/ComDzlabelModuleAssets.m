e13e32622f22bf9b968254d3d5787cdaa1f2b59f
Nikhil <nikhlavida@gmail.com>
2012-10-04 16:51:01 +0530

    Working Project.

---
+++ Classes/ComDzlabelModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComDzlabelModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation ComDzlabelModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
