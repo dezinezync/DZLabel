86f2c4389dc5836d44a86716e9981a25d38fc10e
Nikhil <nikhlavida@gmail.com>
2012-10-04 14:32:09 +0530

    Initial Commit. Using default example from modules to draw red square as view from module.

---
+++ Classes/ComDzlabelView.h
@@ -0,0 +1,14 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2012 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+#import "TiUIView.h"
+#import "RTLabel.h"
+
+@interface ComDzlabelView : TiUIView {
+    UIView *square;
+}
+
+@end
---
+++ example/app.js
@@ -0,0 +1,33 @@
+// This is a test harness for your module
+// You should do something interesting in this harness 
+// to test out the module and to provide instructions 
+// to users on how to use it by example.
+
+
+// open a single window
+var win = Ti.UI.createWindow({
+	backgroundColor:'white'
+});
+var label = Ti.UI.createLabel({
+	top: 20
+});
+win.add(label);
+win.open();
+
+// TODO: write your module tests here
+var dzlabel = require('com.dzlabel');
+Ti.API.info("module is => " + dzlabel);
+
+var foo = dzlabel.createView({
+  "color":"red",
+  "width":20,
+  "height":20
+});
+
+win.add(foo);
+
+label.text = dzlabel.example();
+
+Ti.API.info("module exampleProp is => " + dzlabel.exampleProp);
+dzlabel.exampleProp = "This is a test value";
+
