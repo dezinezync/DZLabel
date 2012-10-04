fec9fc7ed452d9694c14651d8066e9b4b9d00d1b
Nikhil <nikhlavida@gmail.com>
2012-10-04 23:24:13 +0530

    -Now using RTLabel as a submodule -Test cases in app.js - TableView to show test-cases all together instead of one by one. - Some methods requiring implementation have been marked as TODO. - Updated package included.

--- ComDzlabelView.m
+++ ComDzlabelView.m
@@ -6,6 +6,7 @@
  */
 
 #import "ComDzlabelView.h"
+#import "TiUtils.h"
 
 @implementation ComDzlabelView
 
@@ -19,7 +20,10 @@
 {
     if (label == nil)
     {
-        label = [[RTLabel alloc] initWithFrame: [self bounds]];
+        label = [[RTLabel alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+        
+        [label setFont:[UIFont fontWithName:@"Helvetica" size:12]];
+        [label setTextColor:[UIColor blackColor]];
         [self addSubview:label];
     }
     return label;
@@ -33,11 +37,36 @@
     }
 }
 
+//Code from RTLabel Example
+- (void)layoutSubviews
+{
+	[super layoutSubviews];
+    
+	CGSize optimumSize = [self.label optimumSize];
+	CGRect frame = [self.label frame];
+	frame.size.height = (int)optimumSize.height+5; // +5 to fix height issue, this should be automatically fixed in iOS5
+	[self.label setFrame:frame];
+}
+
 -(void)setText_:(id)text {
     ENSURE_STRING_OR_NIL(text)
     NSLog(text);
 
     [self.label setText:text];
 }
+
+/* TO DO */
+// Implement methods to set Font Name, Font Size and Text Color
+
+//Send click event data when a URL is clicked
+// Needs proper implementation
+/*
+- (void)label:(id)label didSelectLinkWithURL:(NSURL *)url {
+    NSDictionary * dict = [NSDictionary dictionaryWithObjectsAndKeys:
+                           url, @"url",
+                           nil];
+    [self.proxy fireEvent:@"click" withObject:dict];
+}
+*/
 
 @end
--- example/app.js
+++ example/app.js
@@ -8,28 +8,59 @@
 var win = Ti.UI.createWindow({
 	backgroundColor:'white'
 });
-var label = Ti.UI.createLabel({
-	top: 20
-});
-win.add(label);
-
 
 // TODO: write your module tests here
 var dzlabel = require('com.dzlabel');
 Ti.API.info("module is => " + dzlabel);
 
-label.text = dzlabel.example();
+//Test cases
+var mockData = [];
+mockData.push("<b>bold</b> and <i>italic</i> style");
+mockData.push("Facebook is a social utility that connects people with friends and others who work , study and live around them. People use Facebook to keep up with friends. And now this is some dummy text for real.");
+mockData.push("<a href='http://example.com/'>link</a>");
+mockData.push("<font face='HelveticaNeue-CondensedBold' size=20><u color=blue>underlined</u> <uu color=red>text</uu></font>");
+mockData.push("<font face='HelveticaNeue-CondensedBold' size=20 color='#CCFF00'>Text with</font> <font face=AmericanTypewriter size=16 color=purple>different colours</font> <font face=Futura size=32 color='#dd1100'>and sizes</font>");
+mockData.push("<font face='HelveticaNeue-CondensedBold' size=20 stroke=1>Text with strokes</font>");
+mockData.push("<a href='http://dezinezync.com/'>@dezinezync</a>");
+/* Test cases below crash the app*/
 
-Ti.API.info("module exampleProp is => " + dzlabel.exampleProp);
-dzlabel.exampleProp = "This is a test value";
-
-var foo = dzlabel.createView({
-	width: Ti.UI.FILL,
-	backgroundColor: 'blue',
-	height: 100,
-	text: "<b>bold</b>,<i>italic</i> and <u>underlined</u> text, and <font face='HelveticaNeue-CondensedBold' size=20 color='#CCFF00'>text with custom font and color</font>"
+var tableView = Ti.UI.createTableView({
+	selectionStyle: 0
 });
 
-win.add(foo);
+var row, foo, data = [];
+
+for(i=0; i<mockData.length; i++) {
+	row = Ti.UI.createTableViewRow({
+		height: Ti.UI.SIZE, // This needs a better implementation. Maybe get size.height of foo on postlayout and use that for the cell height.
+		className: 'row'
+	});
+
+		foo = dzlabel.createView({
+			width: Ti.UI.FILL, //width must always be defined for this to work. If width is set to SIZE, it doesn't draw.
+			height: Ti.UI.SIZE,
+			color: 'black',
+			backgroundColor: 'transparent',
+			text: mockData[i]
+		});
+
+		//foo.addEventListener('postlayout', fooPostLayout);
+
+	row.add(foo);
+
+	data.push(row);
+
+	foo = null; row = null;
+}
+
+tableView.setData(data);
+data = null;
+
+/* This needs implementation */
+/*foo.addEventListener('click', function(e) {
+	console.log(e)
+});*/
+
+win.add(tableView);
 
 win.open();
